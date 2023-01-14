111392f647571f6e11c2e2ab435016df7f9a3365
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-01-14 01:29:01 +0100

    port 3000

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ app.get("/download", async (req, res) => {
 	});
 });
 
-app.listen(3001, () => {
-	console.log("Server running on port 3001");
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
 });
